c4ed837074bfe55628dd164e62ad0048504352c4
Dirk Link <dlink87@MacBook-Pro.local>
2015-05-18 15:17:46 -0400

    All done

---
+++ triangle_test.rb
@@ -0,0 +1,64 @@
+# 1. Copy this file from the course notes repo into a new repository
+# 2. Run `ruby triangle_test.rb`. You should see 6 error messages.
+# 3. Implement the Triangle class until all 6 tests are passing.
+require "minitest/autorun"
+
+class Triangle
+  def initialize x, y, z
+    @side1 = x
+    @side2 = y
+    @side3 = z
+  end
+
+  def kind 
+    if @side1==@side2 and @side1 == @side3
+      return :equilateral
+    end
+
+    if @side1 == @side2 or @side1 == @side3 or @side2 == @side3
+      return :isosceles
+    end
+
+    if @side1 != @side2 or @side1 != @side3 or @side2 =! @side3
+      return :scalene
+    end
+
+  end
+  def perimeter
+    return @side1+@side2+@side3
+  end
+
+
+end
+
+class TestMeme < Minitest::Test
+  def test_equilateral
+    t = Triangle.new(5,5,5)
+    assert_equal :equilateral, t.kind
+  end
+
+  def test_isosceles
+    s = Triangle.new(2,2,3)
+    assert_equal :isosceles, s.kind
+  end
+
+  def test_other_isosceles
+    t = Triangle.new 3,4,3
+    assert_equal :isosceles, t.kind
+  end
+
+  def test_scalene
+    t = Triangle.new 6,7,8
+    assert_equal :scalene, t.kind
+  end
+
+  def test_perimeter_right
+    t = Triangle.new(3,4,5)
+    assert_equal 12, t.perimeter
+  end
+
+  def test_perimeter_eq
+    t = Triangle.new 10,20,30
+    assert_equal 60, t.perimeter
+  end
+end
